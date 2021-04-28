774a11c22f2eeaa7d863433df6415ea1f64fbded
Rodrigo Nogueira <rnandrade02@gmail.com>
2021-04-27 23:08:50 -0300

    func de criar e deletar

---
+++ menuCadastro/menuCadProdutos.py
@@ -0,0 +1,22 @@
+# Iniciando estudos em python. projeto de um crud para cadastrar produtos sem banco de dados só com array/list
+
+saidamenu = False
+
+while saidamenu == False:
+    print("------- Cadastro de Produtos -------")
+    print("\n       ------- Menu -------")
+    print("Informe um número para acessar o menu...")
+    menu = int(input(
+        "1 ---- NOVO PRODUTO \n2 ---- DELETAR  \n3 ---- EDITAR  \n4 ---- PROCURAR \n5 ---- LISTAR TODOS \n6 ---- SAIR\n"))
+    if menu == 1:
+        print("Menu")
+    elif menu == 2:
+        print("Menu")
+    elif menu == 3:
+        print("Menu")
+    elif menu == 4:
+        print("Menu")
+    elif menu == 5:
+        print("Menu")
+    elif menu == 6:
+        saidamenu = True
